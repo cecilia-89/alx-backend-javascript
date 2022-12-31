05cf7aa4701000afd4458b95c3ea9d76e821740d
cecilia-89 <catabong89@gmail.com>
2022-12-31 07:27:05 +0100

    corrections

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const new_array = array;
+  const list = array;
   for (const idx of array) {
-    new_array[array.indexOf(idx)] = appendString + idx;
+    list[array.indexOf(idx)] = appendString + idx;
   }
 
-  return new_array;
+  return list;
 }
