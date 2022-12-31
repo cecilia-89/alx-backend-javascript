625efa293b359835ff9fff1a6c210c8828e2e980
cecilia-89 <catabong89@gmail.com>
2022-12-31 21:12:37 +0100

    added export keyword

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -8,7 +8,7 @@ export default getFullResponseFromAPI = (bool) => {
         }
       );
     } else {
-      reject('The fake API is not working currently');
+      reject(new Error('The fake API is not working currently'));
     }
   });
 };
