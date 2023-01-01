2dbf22c148e5c605cb1311289ed078a299866d76
cecilia-89 <catabong89@gmail.com>
2023-01-01 08:29:37 +0100

    eslint

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,10 +1,9 @@
-/* added comment to top of file */
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({
-        'body': 'Success',
-        'status': 200,
+        body: 'Success',
+        status: 200,
       });
     }
     reject(new Error('The fake API is not working currently'));
