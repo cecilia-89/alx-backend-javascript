ccd830eab374f5ea75c8cf1a12ff89a6d8da409b
cecilia-89 <catabong89@gmail.com>
2023-01-02 05:41:24 +0100

    added new tasks

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { createUser, uploadPhoto } from './utils';
+import { createUser, uploadPhoto } from './utils.js';
 
 export default function handleProfileSignup() {
   Promise.all([createUser(), uploadPhoto()])
@@ -9,3 +9,5 @@ export default function handleProfileSignup() {
       console.log('Signup system offline');
     });
 }
+
+handleProfileSignup();
