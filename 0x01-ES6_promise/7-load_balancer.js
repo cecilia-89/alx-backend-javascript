c1badf9a073702ef6973a633c0280edcbccb642e
cecilia-89 <catabong89@gmail.com>
2023-01-02 15:16:45 +0100

    Es6 Promises

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,6 @@
+// export default
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise.race([chinaDownload, USDownload]).then((result) => {
+    return result
+   })
+}
