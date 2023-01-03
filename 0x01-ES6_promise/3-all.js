ddbd91af31d429490ad09a0ef73e7e81d77be220
cecilia-89 <catabong89@gmail.com>
2023-01-03 10:00:18 +0100

    es6 promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { createUser, uploadPhoto } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([createUser(), uploadPhoto()])
+  return Promise.all([createUser(), uploadPhoto()])
     .then((data) => {
       console.log(`${data[1].body} ${data[0].firstName} ${data[0].lastName}`);
     })
