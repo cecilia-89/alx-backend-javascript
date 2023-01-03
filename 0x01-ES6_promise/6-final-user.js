17cca4db622ec4bc18ba9ed1c2f59b8e33736913
cecilia-89 <catabong89@gmail.com>
2023-01-03 10:22:21 +0100

    es6 promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,21 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [
     signUpUser(firstName, lastName),
-    uploadPhoto(filename),
+    uploadPhoto(fileName),
   ];
-  return Promise.allSettled(promises);
+  const list = [];
+  return Promise.allSettled(promises)
+  .then((results) => {
+    results.forEach((result) => {
+      if (result.status === 'fulfilled') {
+        list.push({ status: result.status, value: result.value });
+      } else {
+        list.push({ status: result.status, value: `${result.reason}` });
+      }
+    });
+    return list
+  })
 }
