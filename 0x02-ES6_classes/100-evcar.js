0402f17257727266f9f130c5c869b9a965aef306
cecilia-89 <catabong89@gmail.com>
2023-01-05 22:03:57 +0100

    es6 classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,6 +1,6 @@
 import Car from './10-car.js';
 
-class EVCar extends Car {
+export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
     this._range = range;
@@ -10,9 +10,3 @@ class EVCar extends Car {
     return new Car();
   }
 }
-
-const ec1 = new EVCar('Tesla', 'Turbo', 'Red', '250');
-console.log(ec1);
-
-const ec2 = ec1.cloneCar();
-console.log(ec2);
