d07492f7d1d57edce8a148f3350671bdc5201491
cecilia-89 <catabong89@gmail.com>
2023-02-21 22:05:26 +0100

    NodeJs Basics

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -5,15 +5,15 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  const req_url = url.parse(req.url).pathname
-  if (req_url == '/') {
+  const reqUrl = url.parse(req.url).pathname;
+  if (reqUrl === '/') {
     res.write('Hello Holberton School!');
     res.end();
   }
 
-  if (req_url == '/students') {
-    res.write('This is the list of our students')
-    res.end()
+  if (reqUrl === '/students') {
+    res.write('This is the list of our students');
+    res.end();
   }
 });
 
